fac8b17a66c019a1c288bb3716c06658bbe557e3
Real <swrely@qq.com>
2022-11-19 12:26:39 +0800

    first commit

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ DEFAULT_HEADERS = {
 """
 调试用 COOKIE
 """
-TEST_COOKIE = '__ckguid=ueT64jfPbEPGYGKgJFyXN4; device_id=19990787331652017355977469151c06fbecc5b1cd27742516f9223705; r_sort_type=score; __jsluid_s=9d44719316f1f1c53a3cc554a35e72cb; smzdm_user_source=812B4E3B3875A91085105626C658C258; shequ_pc_sug=b; homepage_sug=c; _zdmA.vid=*; footer_floating_layer=0; ad_date=19; bannerCounter=%5B%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%5D; ad_json_feed=%7B%7D; sess=BA-hypXIzrI%2Fq3H2mDADD5onqVoaYT0Ceg0DghQlP0i65rgagTbXHw9bSM5oigQ%2FKNj9QMTKIisJVlNm23Xsoq7kuUeC0f5euT4N2cwJqmNLLBdQWrFmTaE6gwZ; user=user%3A7012514392%7C7012514392; smzdm_id=7012514392; _zdmA.uid=ZDMA.W4Lsg24uZ.1668829158.2419200; _zdmA.time=1668829188393.0.https%3A%2F%2Fwww.smzdm.com%2F'
+TEST_COOKIE = ''
 
 
 """
